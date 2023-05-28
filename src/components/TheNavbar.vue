fd86f383a36d8a3a0661c957de94f0adaa3de008
0xjoshva <joshuasteed123@gmail.com>
2023-05-29 00:09:25 +0200

    new push

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <div class="logo" v-if="isSidebarOpen === false">
-      <img src="../assets/logo-light.svg" alt="" />
+      <img src="../assets/logo-light.svg" alt="" v-if="isDark === true" />
+      <img src="../assets/logo-dark.svg" alt="" v-if="isDark === false" />
     </div>
     <div class="placeholder" v-if="isSidebarOpen === true"></div>
     <div class="right-nav">
@@ -17,59 +18,88 @@
     </div>
   </nav>
 
+
+
+
+
   <div :class="['sidebar', { 'slide-in': isSidebarOpen }]">
     <div class="inner-sidebar">
-    <div class="start">
-    <div class="s-logo">
-      <img src="../assets/logo-light.svg" alt="" />
-    </div>
-    <p class="all-boards" v-for="board in boards" :key="board.name">ALL BOARDS ({{board.length}})</p>
-    <div class="nav-items" v-for="board in boards" :key="board.name">
-      <div class="board-btn" v-for="item in board" :key="item.name">
-        <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z"
-          />
-        </svg>
-        {{ item.name }}
+      <div class="start">
+        <div class="s-logo">
+          <img src="../assets/logo-light.svg" alt="" v-if="isDark === true" />
+          <img src="../assets/logo-dark.svg" alt="" v-if="isDark === false" />
         </div>
-    </div>
-    <button class="board-btn create-new-board">
-      <img src="../assets/icon-board.svg" alt="" />
-      <!-- <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="" d="M7.368 12V7.344H12V4.632H7.368V0H4.656v4.632H0v2.712h4.656V12z"/></svg> -->
-      + Create New Board
-    </button>
-  </div>
-  <div class="end">
-    <div class="toggle-theme">
+        <p class="all-boards" v-for="board in boards" :key="board.name">
+          ALL BOARDS ({{ board.length }})
+        </p>
+        <div class="nav-items" v-for="board in boards" :key="board.name">
+          <div class="board-btn" v-for="item in board" :key="item.name">
+            <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z"
+              />
+            </svg>
+            {{ item.name }}
+          </div>
+        </div>
+        <button class="board-btn create-new-board">
+          <img src="../assets/icon-board.svg" alt="" />
+          <!-- <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg"><path fill="" d="M7.368 12V7.344H12V4.632H7.368V0H4.656v4.632H0v2.712h4.656V12z"/></svg> -->
+          + Create New Board
+        </button>
+      </div>
+      <div class="end">
+        <div class="toggle-theme">
+          <img src="../assets/icon-dark-theme.svg" alt="" />
 
+          <div class="toggler">
+            <input
+              type="checkbox"
+              id="toggle"
+              class="offscreen"
+              @click="toggleTheme()"
+            />
+            <label for="toggle" class="switch"></label>
+          </div>
+          <img src="../assets/icon-light-theme.svg" alt="" />
+        </div>
+        <button class="hide-sidebar" @click="toggleSidebar()">
+          <img src="../assets/icon-hide-sidebar.svg" alt="" /> Hide Sidebar
+        </button>
+      </div>
     </div>
-    <button class="hide-sidebar" @click="toggleSidebar()"><img src="../assets/icon-hide-sidebar.svg" alt=""> Hide Sidebar</button>
-  </div>
-  </div>
-  <button v-if="isSidebarOpen === false" @click="toggleSidebar()" class="show-sidebar"><img src="../assets/icon-show-sidebar.svg" alt=""></button>
+    <button
+      v-if="isSidebarOpen === false"
+      @click="toggleSidebar()"
+      class="show-sidebar"
+    >
+      <img src="../assets/icon-show-sidebar.svg" alt="" />
+    </button>
   </div>
 </template>
 <script>
 import dataJSON from "/data/data.json";
-export default{
+export default {
   data() {
     return {
       boards: dataJSON,
       isSidebarOpen: false,
+      isDark: true,
     };
   },
   methods: {
     toggleSidebar() {
       this.isSidebarOpen = !this.isSidebarOpen;
-      console.log(this.boards)
+    },
+    toggleTheme() {
+      this.isDark = !this.isDark;
     },
   },
   computed: {
     itemCount() {
       return this.boards.board.length;
-    }
-  }
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -92,18 +122,30 @@ $HeadingL: 18px;
 $HeadingM: 15px;
 $HeadingS: 12px;
 
+$PrimaryColor: $DarkGrey;
+$PrimaryLineColor: $LinesDark;
+
+.theme-dark {
+  --bg-primary: $DarkGrey;
+  --lines: $LinesDark;
+}
+
+.theme-light {
+  --bg-primary: $White;
+  --lines: $LinesLight;
+}
 nav {
   width: 100%;
   height: 97px;
-  background: $DarkGrey;
-  border-bottom: 1px solid $LinesDark;
+  background: $PrimaryColor;
+  border-bottom: 1px solid $PrimaryLineColor;
   display: flex;
   z-index: 1;
   position: fixed;
 
   .logo {
     width: 210px;
-    border-right: $LinesDark 1px solid;
+    border-right: $PrimaryLineColor 1px solid;
     height: 100%;
     display: flex;
     justify-content: center;
@@ -159,42 +201,41 @@ nav {
   position: absolute;
   width: fit-content;
   height: 100%;
- background:transparent;
+  background: transparent;
   z-index: 10;
   transform: translateX(-91%);
   transition: transform 0.4s ease;
-display: flex;
-justify-content: flex-end;
-align-items: flex-end;
-
-  .show-sidebar{
-      background: $Purple;
-      border:none;
-      outline:none;
-      width:fit-content;
-      height:fit-content;
-      padding: 1rem;
-      padding-left: 13rem;
-      border-top-right-radius: 26px;
-      border-bottom-right-radius: 26px;
-      margin-bottom: 2.5rem;
-      animation: slide-in 1s ease;
-      cursor: pointer;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-
-.inner-sidebar{
-width: 300px;
-height: 100%;
- background: $DarkGrey;
-  border-right: 1px solid $LinesDark;
   display: flex;
-  justify-content: space-between;
-  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-end;
 
-}
+  .show-sidebar {
+    background: $Purple;
+    border: none;
+    outline: none;
+    width: fit-content;
+    height: fit-content;
+    padding: 1rem;
+    padding-left: 13rem;
+    border-top-right-radius: 26px;
+    border-bottom-right-radius: 26px;
+    margin-bottom: 2.5rem;
+    animation: slide-in .5s ease;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .inner-sidebar {
+    width: 300px;
+    height: 100%;
+    background: $PrimaryColor;
+    border-right: 1px solid $PrimaryLineColor;
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+  }
   .s-logo {
     height: 97px;
     width: 100%;
@@ -271,21 +312,29 @@ height: 100%;
     fill: $Purple !important;
   }
 
-  .end{
-    height:fit-content;
+  .end {
+    height: fit-content;
     width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     padding-bottom: 2rem;
-    row-gap: .5rem;
-    .toggle-theme{
+    row-gap: 0.5rem;
+    .toggle-theme {
       background: $VeryDarkBG;
       width: 251px;
       height: 48px;
       border-radius: 6px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      column-gap: 1rem;
+
+      .toggler {
+        transform: translateY(3px);
+      }
     }
-    .hide-sidebar{
+    .hide-sidebar {
       display: flex;
       align-items: center;
       justify-content: start;
@@ -294,13 +343,12 @@ height: 100%;
       color: $MediumGrey;
       font-size: $HeadingM;
       font-weight: bold;
-      column-gap: .5rem;
+      column-gap: 0.5rem;
       background: transparent;
       border: none;
       outline: none;
       cursor: pointer;
     }
-  
   }
 }
 .slide-in {
@@ -320,12 +368,48 @@ height: 100%;
   }
 }
 
-@keyframes slide-in{
-  0%{
+@keyframes slide-in {
+  0% {
     transform: translateX(-400%);
   }
-  100%{
+  100% {
     transform: translateX(0);
   }
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 42px;
+  height: 22px;
+  background-color: $Purple;
+  border-radius: 20px;
+  transition: all 0.3s;
+  cursor: pointer;
+}
+
+.switch::after {
+  content: "";
+  position: absolute;
+  width: 14px;
+  height: 14px;
+  border-radius: 14px;
+  background-color: white;
+  top: 4px;
+  left: 4px;
+  transition: all 0.3s;
+}
+
+input[type="checkbox"]:checked + .switch::after {
+  transform: translateX(20px);
+}
+
+input[type="checkbox"]:checked + .switch {
+  background-color: $Purple;
+}
+
+.offscreen {
+  position: absolute;
+  left: -9999px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,19 @@
 <template>
   <main>
-   {{ boards.name }}
+    <div class="categories">
+      <div class="todo categorie">
+        <div class="circle"></div>
+        <h3 class="header">TODO</h3>
+      </div>
+      <div class="doing categorie">
+        <div class="circle"></div>
+        <h3 class="header">DOING</h3>
+      </div>
+      <div class="done categorie">
+        <div class="circle"></div>
+        <h3 class="header">DONE</h3>
+      </div>
+    </div>
   </main>
 </template>
 <script>
@@ -12,42 +25,83 @@ export default {
       boards: dataJSON,
     };
   },
-  mounted() {
-  },
-  methods: {
-    
-  },
+  mounted() {},
+  methods: {},
 };
 </script>
 <style lang="scss">
-$VeryDarkBG: #20212C;
-$DarkGrey: #2B2C37;
-$LinesDark: #3E3F4E;
-$MediumGrey: #828FA3;
-$LinesLight: #E4EBFA;
-$LightGreyLightBG: #F4F7FD;
+$VeryDarkBG: #20212c;
+$DarkGrey: #2b2c37;
+$LinesDark: #3e3f4e;
+$MediumGrey: #828fa3;
+$LinesLight: #e4ebfa;
+$LightGreyLightBG: #f4f7fd;
 
 $Black: #000112;
-$White: #FFFFFF;
-$Purple: #635FC7;
-$PurpleHover: #A8A4FF;
-$Red: #EA5555;
-$RedHover: #FF9898;
+$White: #ffffff;
+$Purple: #635fc7;
+$PurpleHover: #a8a4ff;
+$Red: #ea5555;
+$RedHover: #ff9898;
 
 $HeadingXL: 24px;
 $HeadingL: 18px;
 $HeadingM: 15px;
 $HeadingS: 12px;
 
-
 main {
+  padding-top: 120px;
+  padding-left: 2rem;
   width: 100%;
   height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  flex-direction: row;
   background: $VeryDarkBG;
+  .categories {
+    width: 100%;
+    height: fit-content;
+    display: flex;
+    .categorie {
+      width: 280px;
+    }
+    .todo {
+      display: flex;
+      align-items: center;
+      column-gap: 0.6rem;
 
+      .circle {
+        background: #49c4e5;
+      }
+    }
+    .doing {
+      display: flex;
+      align-items: center;
+      column-gap: 0.6rem;
+
+      .circle {
+        background: #8471f2;
+      }
+    }
+    .done {
+      display: flex;
+      align-items: center;
+      column-gap: 0.6rem;
+
+      .circle {
+        background: #67e2ae;
+      }
+    }
+  }
+}
+.header {
+  font-size: $HeadingS;
+  color: $MediumGrey;
+  letter-spacing: 2.4px;
+  font-weight: bold;
+}
+.circle {
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
 }
 </style>
